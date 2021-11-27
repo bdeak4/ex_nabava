ee849b74ff80cffea1c74ad848a0a8800e4652e4
Bartol Deak <b@bdeak.net>
2021-11-28 00:43:15 +0100

    publish package

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule ExNabava.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      source_url: "https://github.com/bdeak4/ex_nabava"
     ]
   end
 
@@ -22,9 +25,21 @@ defmodule ExNabava.MixProject do
   defp deps do
     [
       {:httpoison, "~> 1.8"},
-      {:jason, "~> 1.2"}
+      {:jason, "~> 1.2"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    "ExNabava is unofficial elixir lib for accessing nabava.net public data."
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/bdeak4/ex_nabava"}
     ]
   end
 end
